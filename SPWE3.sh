6b24e3398554d5bb5470b0a6108c71383e7b840c
yangalan123 <1049909407@qq.com>
2017-10-02 10:49:59 +0800

    backup for SPWE

---
+++ SPWE3.sh
@@ -0,0 +1,20 @@
+#python SPWE_GAP.py train_data train_hownet hownet.txt_test output_SPWE_gap 2.0 0.1
+#python scorer.py output_SPWE_gap hownet.txt_answer >> report_SPWE_gap
+#python SPWE_GAP.py train_data train_hownet hownet.txt_test output_SPWE_gap_2_02 2.0 0.2 near_SPWE_2_02
+python scorer.py output_SPWE_gap_2_02 hownet.txt_answer >> report_SPWE_gap
+#python SPWE_GAP.py train_data train_hownet hownet.txt_test output_SPWE_gap_2_021 2.0 0.21 near_SPWE_2_02
+python scorer.py output_SPWE_gap_2_021 hownet.txt_answer >> report_SPWE_gap
+#python SPWE_GAP.py train_data train_hownet hownet.txt_test output_SPWE_gap_2_022 2.0 0.22 near_SPWE_2_02
+python scorer.py output_SPWE_gap_2_022 hownet.txt_answer >> report_SPWE_gap
+#python SPWE_GAP.py train_data train_hownet hownet.txt_test output_SPWE_gap_2_023 2.0 0.23 near_SPWE_2_02
+python scorer.py output_SPWE_gap_2_023 hownet.txt_answer >> report_SPWE_gap
+#python SPWE_GAP.py train_data train_hownet hownet.txt_test output_SPWE_gap_2_024 2.0 0.24 near_SPWE_2_02
+python scorer.py output_SPWE_gap_2_024 hownet.txt_answer >> report_SPWE_gap
+#python SPWE_GAP.py train_data train_hownet hownet.txt_test output_SPWE_gap_2_025 2.0 0.25 near_SPWE_2_02
+python scorer.py output_SPWE_gap_2_025 hownet.txt_answer >> report_SPWE_gap
+#python SPWE_GAP.py train_data train_hownet hownet.txt_test output_SPWE_gap_2_026 2.0 0.26 near_SPWE_2_02
+python scorer.py output_SPWE_gap_2_026 hownet.txt_answer >> report_SPWE_gap
+#python SPWE_GAP.py train_data train_hownet hownet.txt_test output_SPWE_gap_origin 1.0 0 near_SPWE_origin
+#python scorer.py output_SPWE_gap hownet.txt_answer >> report_SPWE_gap
+#python SPWE_GAP.py train_data train_hownet hownet.txt_test output_SPWE_gap 2.0 0.3
+#python scorer.py output_SPWE_gap hownet.txt_answer >> report_SPWE_gap
